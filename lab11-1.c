327c2198b7e706355d7a58bb9e64825555441878
troywein <45960380+troywein@users.noreply.github.com>
2024-07-15 15:47:14 -0600

    update f24

---
+++ lab11-1.c
@@ -0,0 +1,19 @@
+/** lab11-1.c
+* ===========================================================
+* Name: Name, Date
+* Section:
+* Project: Lab 11-1
+* Purpose: 
+* ===========================================================
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    // Step 1:  Ask for the House Name
+
+    // Step 2:  Create a Switch Statement
+
+    return 0;
+}
